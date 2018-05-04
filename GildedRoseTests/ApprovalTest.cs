4e7389b7a2371aecb85b0c3781b07e9f528fd30e
William Li <wul@microsoft.com>
2018-05-04 11:57:03 -0700

    Add project files.

---
+++ GildedRoseTests/ApprovalTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace csharp
+{
+    public class ApprovalTest
+    {
+        [Fact]
+        public void ThirtyDays()
+        {
+            StringBuilder fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader("a\n"));
+
+            Program.Main(new string[] { });
+            string output = fakeoutput.ToString();
+
+            var expected = File.ReadAllText("ApprovalTest.ThirtyDays.approved.txt");
+            Assert.Equal(expected, output);
+        }
+    }
+}
